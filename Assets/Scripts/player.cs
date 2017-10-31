42d2fbd0fbf0f8c4524430a49797e865e66c5659
Cameron Stacy <stacy@cs.fsu.edu>
2017-10-30 21:50:26 -0400

    Setting up default asssets

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent (typeof (playerController))]
+public class player : MonoBehaviour {
+
+
+	public float moveSpeed = 5;
+	playerController controller;
+
+	void Start () {
+		controller = GetComponent<playerController> ();
+	}
+
+	void Update () {
+		Vector3 moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+		controller.Move (moveVelocity);
+	}
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent (typeof (Rigidbody))]
+public class playerController : MonoBehaviour {
+
+	Vector3 velocity;
+	Rigidbody _rigidbody;
+
+	void Start () {
+		_rigidbody = GetComponent<Rigidbody> ();	
+	}
+	
+	public void Move(Vector3 v){
+		velocity = v;
+	}
+
+	public void FixedUpdate(){
+		_rigidbody.MovePosition (_rigidbody.position + velocity * Time.fixedDeltaTime);
+	}
+}
