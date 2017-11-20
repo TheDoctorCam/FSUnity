4ec2848fc391b4d8c4a3c1796d5297a84487e8a2
rw15e <rw15e@my.fsu.edu>
2017-11-20 17:56:17 -0500

    implemented GameOver screen

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameUI : MonoBehaviour {
+
+	public Image fadeScreen;
+	public GameObject gameOverUI;
+
+	void Start () {
+		FindObjectOfType<player> ().enemyDeath += endGame;
+	}
+
+	void endGame() { //  will fade screen to black and show the game over message
+		StartCoroutine(Fade (Color.clear, Color.black,1));
+		gameOverUI.SetActive(true);
+	}
+	IEnumerator Fade(Color from, Color to, float time) { // handles the fading of the screen
+		float speed = 1 / time;
+		float percent = 0;
+
+		while (percent < 1) {
+			percent += Time.deltaTime * speed;
+			fadeScreen.color = Color.Lerp(from,to,percent);
+			yield return null;
+		}
+	}
+
+	public void StartNewGame() {
+		Application.LoadLevel ("Game");
+	}
+}
